64a1422c7943e0bce46885757b4e3bccaf68cb56
slavikovq <barbora.slavikova@student.spsmb.cz>
2025-04-16 10:39:48 +0200

    1.Část

--- TestGame/Game1.cs
+++ TestGame/Game1.cs
@@ -8,6 +8,11 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    
+    Texture2D spriteSheet;
+    Rectangle sourceRect;
+    
+    Vector2 playerPosition = new Vector2(0, 0);
 
     public Game1()
     {
@@ -26,6 +31,8 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        spriteSheet = Content.Load<Texture2D>("spritesheet");
+        sourceRect = new Rectangle(0, 0, 8, 8);
 
         // TODO: use this.Content to load your game content here
     }
@@ -34,7 +41,7 @@ public class Game1 : Game
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
+        
         // TODO: Add your update logic here
 
         base.Update(gameTime);
@@ -43,6 +50,16 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
+        _spriteBatch.Begin();
+        
+        _spriteBatch.Draw(
+            spriteSheet,       
+            playerPosition,     
+            sourceRect, 
+            Color.White         
+        );
+        
+        _spriteBatch.End();
 
         // TODO: Add your drawing code here
 
