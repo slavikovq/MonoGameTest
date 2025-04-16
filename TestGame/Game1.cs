bb13326bfc4e77c3cf1ee9c1c309ccb4112e5454
slavikovq <barbora.slavikova@student.spsmb.cz>
2025-04-16 12:38:24 +0200

    6.Část

--- TestGame/Game1.cs
+++ TestGame/Game1.cs
@@ -8,32 +8,26 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    
+
     Texture2D spriteSheet;
     Rectangle sourceRect;
-    
-    
+
     Vector2 playerPosition = new Vector2(0, 0);
     Vector2 playerVelocity = Vector2.Zero;
-    
-    float gravity = 500f; 
-    float groundY = 400f;
-    float moveSpeed = 100f;
 
+    float gravity = 500f;
+    float moveSpeed = 100f;
     bool isOnGround = false;
-    
+
+    int playerSize = 32;
+
+    Rectangle[] platforms;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
-    }
-
-    protected override void Initialize()
-    {
-        // TODO: Add your initialization logic here
-
-        base.Initialize();
     }
 
     protected override void LoadContent()
@@ -42,61 +36,69 @@ public class Game1 : Game
         spriteSheet = Content.Load<Texture2D>("spritesheet");
         sourceRect = new Rectangle(0, 0, 8, 8);
 
-        // TODO: use this.Content to load your game content here
+        platforms =
+        [
+            new Rectangle(250, 450, 32, 32),
+            new Rectangle(350, 430, 32, 32),
+            new Rectangle(450, 450, 32, 32),
+
+            new Rectangle(250, 200, 32, 32),
+            new Rectangle(350, 200, 32, 32),
+            new Rectangle(450, 200, 32, 32)
+        ];
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-        
+
         float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
         KeyboardState keyboard = Keyboard.GetState();
-        
+
+        Vector2 previousPosition = playerPosition;
+
         if (keyboard.IsKeyDown(Keys.A))
-        {
             playerPosition.X -= moveSpeed * deltaTime;
-        }
+
         if (keyboard.IsKeyDown(Keys.D))
-        {
             playerPosition.X += moveSpeed * deltaTime;
-        }
-        
+
         if ((keyboard.IsKeyDown(Keys.W) || keyboard.IsKeyDown(Keys.Space)) && isOnGround)
         {
             playerVelocity.Y = -250f;
             isOnGround = false;
         }
 
-        
         playerVelocity.Y += gravity * deltaTime;
-        playerPosition += playerVelocity * deltaTime;
-        
+        playerPosition.Y += playerVelocity.Y * deltaTime;
+
+        Rectangle playerRect = new Rectangle(
+            (int)playerPosition.X,
+            (int)playerPosition.Y,
+            playerSize,
+            playerSize
+        );
+
+        isOnGround = false;
+
+
+
         int windowWidth = GraphicsDevice.Viewport.Width;
         int windowHeight = GraphicsDevice.Viewport.Height;
-        
-        if (playerPosition.Y + sourceRect.Height >= windowHeight)
-        {
-            playerPosition.Y = windowHeight - sourceRect.Height;
-            playerVelocity.Y = 0;
-            isOnGround = true;
-        }
-        
+
         if (playerPosition.X < 0)
             playerPosition.X = 0;
 
-        if (playerPosition.X + sourceRect.Width > windowWidth)
-            playerPosition.X = windowWidth - sourceRect.Width;
-        
-        if (playerPosition.Y + sourceRect.Height >= windowHeight)
+        if (playerPosition.X + playerSize > windowWidth)
+            playerPosition.X = windowWidth - playerSize;
+
+        if (playerPosition.Y + playerSize >= windowHeight)
         {
-            playerPosition.Y = windowHeight - sourceRect.Height;
+            playerPosition.Y = windowHeight - playerSize;
             playerVelocity.Y = 0;
+            isOnGround = true;
         }
-
-
-        
-        // TODO: Add your update logic here
 
         base.Update(gameTime);
     }
@@ -105,18 +107,22 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
         _spriteBatch.Begin();
-        
+
         _spriteBatch.Draw(
-            spriteSheet,       
-            playerPosition,     
-            sourceRect, 
-            Color.White         
+            spriteSheet,
+            new Rectangle((int)playerPosition.X, (int)playerPosition.Y, playerSize, playerSize),
+            sourceRect,
+            Color.White
         );
-        
+
+        Rectangle platformMiddleTop = new Rectangle(8, 8, 8, 8);
+
+        foreach (var platform in platforms)
+        {
+            _spriteBatch.Draw(spriteSheet, platform, platformMiddleTop, Color.White);
+        }
+
         _spriteBatch.End();
-
-        // TODO: Add your drawing code here
-
         base.Draw(gameTime);
     }
 }
--- TestGame/obj/Debug/net8.0-windows/TestGame.AssemblyInfo.cs
+++ TestGame/obj/Debug/net8.0-windows/TestGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e08380e7471ca63bdedd038be9edfa1feebcf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecea04dc4eb9c3b7cac122ae2a609d3aa3201ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
