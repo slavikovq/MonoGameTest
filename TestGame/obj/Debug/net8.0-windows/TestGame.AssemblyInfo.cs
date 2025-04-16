ab50a10aa4e7253477e0236c4cd9fbf9b02b475a
slavikovq <barbora.slavikova@student.spsmb.cz>
2025-04-16 14:07:17 +0200

    7.Část

--- TestGame/obj/Debug/net8.0-windows/TestGame.AssemblyInfo.cs
+++ TestGame/obj/Debug/net8.0-windows/TestGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecea04dc4eb9c3b7cac122ae2a609d3aa3201ae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb13326bfc4e77c3cf1ee9c1c309ccb4112e5454")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
